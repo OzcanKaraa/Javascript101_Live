c4679ac2a24484158a73183b078768a31249c9f3
Ozcan Kara <ozcankara810@gmail.com>
2023-04-12 11:53:47 +0300

    some changes

--- scripts/formelements.js
+++ scripts/formelements.js
@@ -1,3 +1,27 @@
-var tbox1;
-tbox1 = document.getElementById("tbox1").value;
-document.getElementById("sonuc1").innetHtml = tbox1;
+﻿function tboxWrite() {
+    var tbox1; // aşağıda kullanağım değişgeni tanımlıyorum.
+
+    tbox1 = document.getElementById("tbox1").value; // html tarafındaki tbox1 elementinin içerğini okuyorum.
+
+    document.getElementById("sonuc1").innerHTML = tbox1; // html tarafındaki sonuc1 id li elementin içeriğini öğrendiğim değerle dolduruyorum.
+}
+
+function artir() {
+    var deger;
+
+    deger = parseInt(document.getElementById("tbox2").value);
+
+    deger++;
+
+    document.getElementById("tbox2").value = deger;
+}
+
+function azalt() {
+    var deger;
+
+    deger = parseInt(document.getElementById("tbox2").value);
+
+    deger--;
+
+    document.getElementById("tbox2").value = deger;
+}
